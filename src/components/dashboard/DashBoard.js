18b33889768c13e3963bc800e12d5fab31110255
Matt Locklin <mattlocklin@live.co.uk>
2020-01-21 23:40:36 +0000

    used front end to find most challenges/challenged player

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { usePlayerChallenges } from '../../helpers/PlayerChallenges';
 import { useGetStats } from '../../helpers/GetStats';
 import { venues, rulesets } from '../../helpers/Data';
 import PieChart from '../display/DonutChart';
@@ -9,9 +8,14 @@ import { Container } from '@material-ui/core';
 export const DashBoard = () => {
 
     const results = useSelector(state => state.results)
-    const challenges = usePlayerChallenges(results)
+    const players = useSelector(state => state.players)
     const stats = useGetStats(results)
+    const obj = stats.getHighestChallenger(players)
+    const mostchallenged = stats.getMostChallenged(players)
     
+    if (!obj) {
+        return <h1>Loading</h1>
+    }
     return (
         <Container>
             <h1>Dashboard</h1>
@@ -24,10 +28,10 @@ export const DashBoard = () => {
                 <h3>Total Games Played: {results.length} </h3>
                 <PieChart stats={stats.getChallengesWon(results)} />
             
-            <h4>Total Prize Money Played for: £{challenges.amountPlayedFor}</h4>
-            <h4>Average Pot: £{Math.round(challenges.amountPlayedFor / results.length)}</h4>
-            <h4>Most Challenged Player: {challenges.mostChallengedPlayer(results)}</h4>
-            <h4>Most Challenges: {challenges.mostChallengesPlayer(results)}</h4>
+            <h4>Total Prize Money Played for: £{stats.amountPlayedFor}</h4>
+            <h4>Average Pot: £{Math.round(stats.amountPlayedFor / results.length)}</h4>
+            <h4>Most Challenged Player: {mostchallenged.name} ({mostchallenged.count})</h4>
+            <h4>Player with most challenges: {obj.name} ({obj.count}) </h4>
         </Container>
     )
 }
--- src/helpers/GetStats.js
+++ src/helpers/GetStats.js
@@ -1,5 +1,7 @@
+import React from 'react'
+
 export const useGetStats = (results) => {
-    // Get venues played and set to format for pie chart ({name: '', value: 1}) 
+    // Get venues or rulesets played and set to format for pie chart ({name: '', value: 1}) 
     const getStats = (stat) => {
 
         let stats = []
@@ -19,6 +21,7 @@ export const useGetStats = (results) => {
         return stats
     }
 
+    // Use results to loop over and return whether challengers or challenged player won
     const getChallengesWon = (results) => {
         let data = []
         let counter = 0
@@ -33,8 +36,77 @@ export const useGetStats = (results) => {
         })
         return data
     }
+    // Get amount Played for total
+    const amountPlayedFor = results.reduce(function (accumulator, currentValue, currentIndex, array) {
+        return accumulator + currentValue.pot
+    }, 0)
+
+    const getHighestChallenger = (players) => {
+        let count = 0
+        let data = []
+        players.forEach(player => {
+            player.results.forEach(result => {
+                if (player.name === result.challenger) {
+                    count = count + 1
+                    const name = player.name
+                    data.push({ name: name, count: count })
+                }
+            })
+            count = 0;
+            return data
+        })
+        data.sort(compareValues('count', 'desc'))
+        return data[0]
+    }
+
+    function compareValues(key, order = 'asc') {
+        return function innerSort(a, b) {
+            if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+                // property doesn't exist on either object
+                return 0;
+            }
+
+            const varA = (typeof a[key] === 'string')
+                ? a[key].toUpperCase() : a[key];
+            const varB = (typeof b[key] === 'string')
+                ? b[key].toUpperCase() : b[key];
+
+            let comparison = 0;
+            if (varA > varB) {
+                comparison = 1;
+            } else if (varA < varB) {
+                comparison = -1;
+            }
+            return (
+                (order === 'desc') ? (comparison * -1) : comparison
+            );
+        };
+    }
+
+    const getMostChallenged = (players) => {
+        let count = 0
+        let data = []
+        players.forEach(player => {
+            player.results.forEach(result => {
+                if (player.name === result.challenged) {
+                    count = count + 1
+                    const name = player.name
+                    data.push({ name: name, count: count })
+                }
+            })
+            count = 0;
+            return data
+        })
+        data.sort(compareValues('count', 'desc'))
+        return data[0]
+    }
+
+    // const max = getmostChallengedPlayer(players)
     return {
         getStats,
-        getChallengesWon
+        getChallengesWon,
+        amountPlayedFor,
+        getHighestChallenger,
+        getMostChallenged
     }
 }
